3803060dae2e7848f1142d51e117ed52855ab86d
Wong Siow Wei <b022210249@student.utem.edu.my>
2024-03-25 12:06:43 +0800

    wong

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
 })
+
+//Path:package.json
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://soklywn2612:<password>@cluster0.isqzhdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
