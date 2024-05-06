b07030641311f599cbb433421030113afb140b68
Wong Siow Wei <b022210249@student.utem.edu.my>
2024-05-06 23:59:03 +0800

    beuty

--- index.js
+++ index.js
@@ -1,92 +1,95 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
-const bcrypt =require('bcrypt');
-const jwt=require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-app.use(express.json())
+app.use(express.json());
 
 //1)new user registration
-app.post('/user',async(req, res)=>{
-    
-  const hash =bcrypt.hashSync(req.body.password,10);
+app.post("/user", async (req, res) => {
+  const hash = bcrypt.hashSync(req.body.password, 10);
 
-    //insetOne
-    let result= await client.db('BERR2243').collection('student').insertOne(
-      {
-        username: req.body.username,
-        password: hash,      //all infos one from body
-        //name: req.body.name,
-        //email:req.body.email  
-    })
-    res.send(result)
-    })
-app.post('/login',async(req, res)=>{
-      //username: req.body.username         //data of user shound keyin
-      //password: req.body.password
+  //insetOne
+  let result = await client.db("BERR2243").collection("student").insertOne({
+    username: req.body.username,
+    password: hash, //all infos one from body
+    //name: req.body.name,
+    //email:req.body.email
+  });
+  res.send(result);
+});
+app.post("/login", async (req, res) => {
+  //username: req.body.username         //data of user shound keyin
+  //password: req.body.password
 
-      //Step1: Check user name if exist
-      let result=await client.db("BERR2243").collection("student").findOne(
-        {username: req.body.username    //pwd alrdy hash
-      })
+  //Step1: Check user name if exist
+  let result = await client.db("BERR2243").collection("student").findOne({
+    username: req.body.username, //pwd alrdy hash
+  });
 
-    if(!result) {res.send("Username not found")}
-    else{
-      //Step2: Check if password is correct
-      if(bcrypt.compareSync(req.body.password,result.password)==true){
-        var token = jwt.sign({_id:result._id , username:result.username},'Super secret passkey',{ expiresIn: 10*60 });
-        res.send(token)
-        //res.send("Login success")
-      }else{
-        res.send("Wrong password")
-      }
+  if (!result) {
+    res.send("Username not found");
+  } else {
+    //Step2: Check if password is correct
+    if (bcrypt.compareSync(req.body.password, result.password) == true) {
+      var token = jwt.sign(
+        { _id: result._id, username: result.username },
+        "Super secret passkey",
+        { expiresIn: 10 * 60 }
+      );
+      res.send(token);
+      //res.send("Login success")
+    } else {
+      res.send("Wrong password");
     }
-    console.log (result)
   }
-)
-
+  console.log(result);
+});
 
 //get user profile
-app.get('/user/:name', async(req, res) => {
-  let result = await client.db('BERR2243').collection('student').findOne({ 
-   name:req.params.name   //name:new ObjectId(req.params.name) ZHAO DOC BY USING ID HAOMA
-  })                      //params jia 'name' as endpoint
- 
-  res.send(result)
-})
+app.get("/user/:name", async (req, res) => {
+  let result = await client.db("BERR2243").collection("student").findOne({
+    name: req.params.name, //name:new ObjectId(req.params.name) ZHAO DOC BY USING ID HAOMA
+  }); //params jia 'name' as endpoint
+
+  res.send(result);
+});
 
 //patch(update) user profile
-app.patch('/user/:name', async(req, res) => {
-  let result = await client.db('BERR2243').collection('student').updateOne(
-    {_id:new ObjectId(req.params.name)},
-    {$set:{name:req.body.name}
-  })
-  res.send(result)
-})
+app.patch("/user/:name", async (req, res) => {
+  let result = await client
+    .db("BERR2243")
+    .collection("student")
+    .updateOne(
+      { _id: new ObjectId(req.params.name) },
+      { $set: { name: req.body.name } }
+    );
+  res.send(result);
+});
 
 //delete user profile
-app.delete('/user/:name', async(req, res) => {
-  let result = await client.db('BERR2243').collection('student').deleteOne()
-    res.send(result)
-  
-})
+app.delete("/user/:name", async (req, res) => {
+  let result = await client.db("BERR2243").collection("student").deleteOne();
+  res.send(result);
+});
 
-app.post('/buy',async(req, res)=>{
+app.post("/buy", async (req, res) => {
   //  console.log(req.headers.authorization.split("")[1])
-  const token=req.headers.authorization.split(" ")[1]
-  var decoded=jwt.verify(token,'Super secret passkey');
-  console.log(decoded)
-})
+  const token = req.headers.authorization.split(" ")[1];
+  var decoded = jwt.verify(token, "Super secret passkey");
+  console.log(decoded);
+});
 
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
-})
-app.get('/') 
-
+  console.log(`Example app listening on port ${port}`);
+});
+app.get("/");
 
 //Path:package.json
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-const uri = "mongodb+srv://soklywn2612:Asdfghjkl2326@cluster0.isqzhdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://soklywn2612:Asdfghjkl2326@cluster0.isqzhdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -94,7 +97,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -102,12 +105,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    console.log('Connected successfully to Mongobd server');
-
+    console.log("Connected successfully to Mongobd server");
   } finally {
     // Ensures that the client will close when you finish/error
     ////await client.close();
   }
 }
 run().catch(console.dir);
-
