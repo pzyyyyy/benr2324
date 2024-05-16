a79a82024e7b520a30c806f04867c849908e5d99
Wong Siow Wei <b022210249@student.utem.edu.my>
2024-05-17 04:20:02 +0800

    Lab

--- LAB/index.js
+++ LAB/index.js
@@ -1,18 +1,11 @@
 async function main() {
   const { MongoClient } = require("mongodb");
-
-  // Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
   const uri =
     "mongodb+srv://b022210255:12345abc@cluster0.wge1mil.mongodb.net/sample_analytics?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
-
   try {
-    // Connect to the MongoDB cluster
     await client.connect();
-
-    // Make the appropriate DB calls
     await listDatabases(client);
-
     // Run the aggregation pipeline to find Leslie Martinez
     await findLeslieMartinez(client);
   } catch (e) {
@@ -23,9 +16,7 @@ async function main() {
 }
 
 async function listDatabases(client) {
-  // Fetch the list of databases in the cluster
   const databasesList = await client.db().admin().listDatabases();
-
   console.log("Databases:");
   databasesList.databases.forEach((db) => {
     console.log(` - ${db.name}`);
@@ -33,35 +24,49 @@ async function listDatabases(client) {
 }
 
 async function findLeslieMartinez(client) {
-  // Get reference to the sample_analytics database
-  const database = client.db("sample_analytics");
-
-  // Get reference to the customers collection
-  const collection = database.collection("customers");
-
-  // Aggregation pipeline to find Leslie Martinez and project required fields
-  const pipeline = [
-    {
-      $match: {
-        name: "Leslie Martinez",
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        name: 1,
-        email: 1,
-        accounts: 1,
-      },
-    },
-  ];
-
   // Execute the aggregation pipeline
-  const result = await collection.aggregate(pipeline).toArray();
+  const resultA = await client
+    .db("sample_analytics")
+    .collection("customers")
+    .aggregate([
+      {
+        $match: {
+          name: "Leslie Martinez",
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          name: 1,
+          email: 1,
+          accounts: 1,
+        },
+      },
+    ])
+    .toArray();
+  const resultB = await client
+    .db("sample_analytics")
+    .collection("customers")
+    .aggregate([
+      {
+        $match: { name: "Leslie Martinez" },
+      },
+      {
+        $lookup: {
+          from: "accounts",
+          localField: "accounts",
+          foreignField: "account_id",
+          as: "accounts",
+        },
+      },
+    ])
+    .toArray();
 
   // Print the result
-  console.log("Leslie Martinez's details:");
-  console.log(result);
+  console.log("Part A-Leslie Martinez's details:");
+  console.log(resultA);
+  console.log("Part B-Leslie Martinez's details:");
+  console.log(resultB);
 }
 
 main().catch(console.error);
