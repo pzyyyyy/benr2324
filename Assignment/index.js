91a2a317bdb3fd7c3a491c0033474cd8df4b85a1
Wong Siow Wei <b022210249@student.utem.edu.my>
2024-06-21 15:37:11 +0800

    ALL DONE

--- Assignment/index.js
+++ Assignment/index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
+
 //API FOR ADMIN
 //login for admin
 app.post("/adminLogin", async (req, res) => {
