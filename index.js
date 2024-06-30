2e0a9073a60f63329f4e54e580b72f5486032929
Wong Siow Wei <b022210249@student.utem.edu.my>
2024-06-30 23:33:26 +0800

    EVERYTHINGS DONE LE DE T_T

--- index.js
+++ index.js
@@ -1618,7 +1618,7 @@ app.get("/leaderboard", verifyToken, async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("FOR BATTLE!! GAME (  ´-ω ･)▄︻┻┳══━一");
+  res.send("FOR BATTLE!! GAME ( -ω ･)▄︻┻┳══━一");
 });
 
 app.listen(port, () => {
