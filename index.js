d681741490dc3d4911d4e85f304d0d0e1ba35b68
Wong Siow Wei <b022210249@student.utem.edu.my>
2024-11-20 23:26:12 +0800

    TLS

--- index.js
+++ index.js
@@ -1619,7 +1619,7 @@ app.get("/leaderboard", verifyToken, async (req, res) => {
 
 app.get("/", (req, res) => {
   res.send(
-    "Well Done!! I done encrypt this messange through TLS!! ( -ω ･)▄︻┻┳══━一"
+    "Well Done!! I done decrypt this messange through TLS!! ( -ω ･)▄︻┻┳══━一"
   );
   //res.send("FOR BATTLE!! GAME ( -ω ･)▄︻┻┳══━一");
 });
